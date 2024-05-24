388ccde3c1f4a4948fd95b2d70ba6061197645ba
Maryam labyed <maryamlabyed4@gmail.com>
2024-05-24 11:30:00 +0100

    add mmessage update and delete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,13 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <div class="container mt-5">
+<div class="container">
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
         @yield('content')
     </div>
 </body>
